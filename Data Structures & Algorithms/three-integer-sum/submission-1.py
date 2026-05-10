f4feeb3d36616598f41f9ee66e97eab7aa380c30
Justin Julius <justintan244@gmail.com>
2026-05-10 23:57:45 +0700

    Add: three-integer-sum - submission-1

---
+++ Data Structures & Algorithms/three-integer-sum/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        combinationMap = defaultdict(list)
+        for i in range(len(nums) - 1):
+            for j in range(i + 1, len(nums), 1):
+                add = nums[i] + nums[j]
+                combinationMap[add].append([i, j])
+        res = []
+        traversedNum = set()
+        for i in range(len(nums)):
+            if nums[i] not in traversedNum and combinationMap.get(-nums[i], 0) != 0:
+                traversedNum.add(nums[i])
+                for idxList in combinationMap[-nums[i]]:
+                    if i not in idxList:
+                        res.append([nums[i], nums[idxList[0]], nums[idxList[1]]])
+        finRes = []
+        for lst in res:
+            lst.sort()
+            if lst not in finRes:
+                finRes.append(lst)
+        return finRes
