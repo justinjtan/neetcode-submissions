f19d2e35b1f4524f7548e65e6771d87241bf6c5b
Justin Julius <justintan244@gmail.com>
2026-06-15 10:38:38 +0700

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        for string in strs:
+            sortedString = "".join(sorted(string))
+            res[sortedString].append(string)
+        return list(res.values())
+        #WCRT: O(N + MLogM) | Space: O(N+M) where M is the largest length of a string in strs
