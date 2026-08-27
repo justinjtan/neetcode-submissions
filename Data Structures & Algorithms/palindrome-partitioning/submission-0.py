8cae4755b9a8d244e5e136923ffad3c78d12d3c8
Justin Julius Tan <justintan244@gmail.com>
2026-08-27 16:14:37 +0700

    Add: palindrome-partitioning - submission-0

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        for i in range(len(s) // 2):
+            if s[i] != s[len(s) - i - 1]:
+                return False
+        return True
+
+    def partition(self, s: str) -> List[List[str]]:
+        res = []
+        stack = []
+
+        def solve(i, curr):
+            if i == len(s):
+                if not curr:
+                    res.append(stack.copy())
+                return
+            curr.append(s[i])
+            if self.isPalindrome(curr):
+                stack.append("".join(curr))
+                solve(i + 1, [])
+                stack.pop()
+            solve(i + 1, curr)
+
+        solve(0, [])
+        return res
+        #WCRT: O(N * 2^N) | Space: O(N) extra space and O(2^N) for output list
