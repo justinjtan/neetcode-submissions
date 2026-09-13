5da5253685dc3b80cb8ba4fed83de160d95f851b
Justin Julius Tan <justintan244@gmail.com>
2026-09-13 09:15:09 -0400

    Add: count-subsequences - submission-0

---
+++ Data Structures & Algorithms/count-subsequences/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        memo = {}
+        
+        def dfs(i, j):
+            if j >= len(t):
+                return 1
+            if i >= len(s):
+                return 0
+            if (i, j) in memo:
+                return memo[(i, j)]
+            res = dfs(i + 1, j)
+            if s[i] == t[j]:
+                res += dfs(i + 1, j + 1)
+            memo[(i, j)] = res
+            return res
+        
+        return dfs(0, 0)
+        #WCRT: O(N * M) | Space: O(N * M) where N is length of s and M is length of t.
